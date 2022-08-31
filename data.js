7ab6e164da690767f503f22eae38f6ca0f0f3939
Ghers Fisman <ghersf91@gmail.com>
2022-08-31 15:44:31 +0200

    data.js created and dataset moved into the file

---
+++ data.js
@@ -0,0 +1,57 @@
+let shopItemsData = [
+    {
+        id: "001",
+        name: "Crunch",
+        price: 1.50,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, voluptates?",
+        img: "images/crunch-bar.jpeg"
+    },
+    {
+        id: "002",
+        name: "Cookies 'n' Creme",
+        price: 2.50,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, voluptates?",
+        img: "images/hersheys-cookies-n-cream-bar.jpeg"
+    },
+    {
+        id: "003",
+        name: "Kit Kat",
+        price: 1.25,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, voluptates?",
+        img: "images/kit-kat-bar.jpeg"
+    },
+    {
+        id: "004",
+        name: "M&M's",
+        price: 1.50,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, voluptates?",
+        img: "images/m-&-m-bag.jpeg"
+    },
+    {
+        id: "005",
+        name: "Mars",
+        price: 1.80,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, voluptates?",
+        img: "images/mars-bar.jpeg"
+    },
+    {
+        id: "006",
+        name: "Milky Way",
+        price: 1.80,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, voluptates?",
+        img: "images/milky-way-bar.jpeg"
+    },
+    {
+        id: "007",
+        name: "Snickers",
+        price: 1.80,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, voluptates?",
+        img: "images/snickers-bar.jpeg"
+    },
+    {
+        id: "008",
+        name: "Twix",
+        price: 1.60,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, voluptates?",
+        img: "images/twix-bar.jpeg"
+    }];
